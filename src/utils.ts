3604df86601542e3f654c4d19c86349f10daabcb
Dalmo Mendonça <dalmo3@gmail.com>
2020-08-19 13:00:51 +1200

    first working version using executeSymbolProvider

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { symlink } from 'fs';
+import { url } from 'inspector';
 import * as vscode from 'vscode';
 
 export const DEBUG = vscode.workspace
@@ -14,24 +16,63 @@ export const logger = {
 };
 
 /**
- * Get symbol tree
- *
- * todo: It's not returning the actual tree, only the top level symbols
- * The top level symbols should have a `children` property but they don't
+ * executeDocumentSymbolProvider returns, according to the API, a promise that resolves to an array of SymbolInformation and DocumentSymbol instances
+ * I was having issues working with either type until I realised the objects actually have properties from both types, so it made sense to create the intersection type for it
  */
-const findAllSymbolsInCurrentDocument = async () => {
-  let allSymbols;
+export type DocumentSymbolInformation = vscode.DocumentSymbol &
+  vscode.SymbolInformation;
+
+// aux function so we only call executeSymbolProvider once
+const findSymbolRecursively = (
+  symbolName: string,
+  docSymbols: DocumentSymbolInformation[]
+): DocumentSymbolInformation | undefined => {
+  const foundSymbol = docSymbols.find((s) => s.name === symbolName);
+  return (
+    foundSymbol ||
+    findSymbolRecursively(
+      symbolName,
+      docSymbols.flatMap((s) => s.children as DocumentSymbolInformation[])
+    )
+  );
+};
+
+/**
+ * this is the main function
+ *
+ * @param symbolName the symbol we're looking for
+ * @param uri the document's uri
+ */
+const findSymbolInDocument = async (symbolName: string, uri: vscode.Uri) => {
+  const docSymbols = await executeDocumentSymbolProvider(uri);
+  return findSymbolRecursively(symbolName, docSymbols);
+};
+
+/**
+ * Get symbol tree
+ * It returns an array with the top level symbols only
+ * then we browse those recursively through their .children property
+ * see: findSymbolRecursively
+ *
+ * @param uri the document's uri
+ */
+const executeDocumentSymbolProvider = async (uri: vscode.Uri) => {
+  let docSymbols;
   try {
-    allSymbols = await vscode.commands.executeCommand(
+    docSymbols = await vscode.commands.executeCommand(
       'vscode.executeDocumentSymbolProvider',
-      vscode.window.activeTextEditor?.document.uri
+      uri
     );
-    logger.log('symbols in current doc.... ', allSymbols);
+    logger.log('symbols in current doc.... ', docSymbols);
   } catch (e) {
     logger.error('Error in executeDocumentSymbolProvider: ', e);
   }
-  return allSymbols as vscode.DocumentSymbol[];
+  return docSymbols as DocumentSymbolInformation[];
 };
+
+//legacy
+const findAllSymbolsInCurrentDocument = () =>
+  executeDocumentSymbolProvider(vscode.window.activeTextEditor?.document.uri!);
 
 /**
  * Navigates to the symbol definition in the current document.
@@ -44,14 +85,20 @@ const findAllSymbolsInCurrentDocument = async () => {
 const goToSymbol = async (symbolName: string) => {
   // find symbol def in current document
   // unused
-  const allSymbols = await findAllSymbolsInCurrentDocument();
-  const symbolDef = allSymbols.find((s) => s.name === symbolName);
+  const docSymbols = await findAllSymbolsInCurrentDocument();
+  // const allSymbols = docSymbols
+  // const symbolDef = docSymbols.find((s) => s.name === symbolName);
+  const symbolDef = findSymbolRecursively(symbolName, docSymbols);
   logger.log('Symbol found: ', symbolDef);
   // if (!symbolDef)
   // return vscode.window.showInformationMessage('No definitions found!');
   // todo: goto symbol declaration
   // if we have the symbol, we have the range and we can go to
   // actually
+
+  if (symbolDef) {
+    vscode.commands.executeCommand('editor.action.goToLocations');
+  }
 
   try {
     // look for the symbol definition in the current document
@@ -69,6 +116,13 @@ const goToSymbol = async (symbolName: string) => {
 };
 
 /**
+ * todo: this should create all Uri's that we'll look for
+ * this function could get really messy as we expand the use cases covered
+ * should return Uri[]
+ */
+const createCandidateArray = () => {};
+
+/**
  *
  * Tries to find the actual definition for a symbol based on its type definition file.
  *
@@ -77,36 +131,68 @@ const goToSymbol = async (symbolName: string) => {
  * @param symbolName the symbol whose definition we're looking for
  * @param typeDefFile the definition file found by vs code, typically <file>.d.ts
  * @param extensions the array of file extensions we'll look for
+ *
+ * todo: swap last parameter for a Uri[] with candidates, and create this array somewhere else
  */
 export const findDefinition = async (
   symbolName: string,
   typeDefFile: vscode.Uri,
   extensions: string[]
-) => {
+): Promise<boolean> => {
   const [ext, ...next] = extensions;
 
   // create new Uri changing path ending from '.d.ts' to '.ts' and try to open it
   const candidate = typeDefFile.with({
     path: typeDefFile.path.slice(0, -4) + ext,
   });
-  let found = false;
-  vscode.window.showTextDocument(candidate).then(
-    async () => {
-      if (DEBUG) vscode.window.showInformationMessage(`Found .${ext} file.`);
+  // let found = Promise.resolve(false);
 
-      // file is open, proceed to look for symbol definition inside of it
-      await goToSymbol(symbolName);
-      found = true;
-    },
-    async () => {
-      if (DEBUG) vscode.window.showErrorMessage(`.${ext} not found.`);
-      //not found, trying next extension
-      if (next.length) {
-        found = await findDefinition(symbolName, typeDefFile, next);
-      } else if (DEBUG) {
-        vscode.window.showErrorMessage(`Defintion file not found.`);
-      }
+  try {
+    // check if file exists, throws if it doesn't
+    await vscode.workspace.fs.stat(candidate);
+    // don't like using Exception for execution flow, but couldn't find another way to check if file exists without using fs.stat
+
+    if (DEBUG) vscode.window.showInformationMessage(`Found .${ext} file.`);
+
+    // proceed to look for symbol definition inside of it
+    const foundsym = await findSymbolInDocument(symbolName, candidate);
+    if (foundsym) {
+      console.log('found symbol', foundsym);
+      // go to locations api reference straight from the source code
+      // https://github.com/microsoft/vscode/blob/8434c86e5665341c753b00c10425a01db4fb8580/src/vs/editor/contrib/gotoSymbol/goToCommands.ts#L719
+      vscode.commands.executeCommand(
+        'editor.action.goToLocations',
+        foundsym.location.uri,
+        foundsym.location.range.start,
+        [],
+      );
     }
-  );
-  return found
+    return !!foundsym;
+  } catch (error) {
+    if (DEBUG) vscode.window.showErrorMessage(`.${ext} not found.`);
+    //not found, trying next extension
+
+    return findDefinition(symbolName, typeDefFile, next);
+  }
+
+  // old method, opened the document first
+  // vscode.window.showTextDocument(candidate).then(
+  //   async () => {
+  //     if (DEBUG) vscode.window.showInformationMessage(`Found .${ext} file.`);
+
+  //     // file is open, proceed to look for symbol definition inside of it
+  //     await goToSymbol(symbolName);
+  //     found = true;
+  //   },
+  //   async () => {
+  //     if (DEBUG) vscode.window.showErrorMessage(`.${ext} not found.`);
+  //     //not found, trying next extension
+  //     if (next.length) {
+  //       found = await findDefinition(symbolName, typeDefFile, next);
+  //     } else if (DEBUG) {
+  //       vscode.window.showErrorMessage(`Defintion file not found.`);
+  //     }
+  //   }
+  // );
+  // return found;
 };
